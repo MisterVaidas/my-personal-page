a74dcb29a32260aa76ef450319e1faaeb4b01a15
VAIDAS SIMKUS <VAIDAS.SIMKUS@YAHOO.COM>
2023-06-16 23:49:21 +0100

    added new feature to app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ alert(`Hello, ${userName}! Welcome to the About Me Guessing Game.`);
 
 // Array of questions and answers
 let questions = [
-    //...previous questions...
+    
     {
         question: "Am I a cat person? (y/n)",
         answer: "y",
@@ -26,7 +26,6 @@ let questions = [
         question: "Am I a morning person? (y/n)",
         answer: "y",
     },
-
     {
         question: "What is my age?",
         answer: "41",
@@ -37,15 +36,19 @@ let questions = [
     },
 ];
 
+// Variable to store the number of correct answers
+let correctAnswers = 0;
+
 // Loop through each question
 questions.forEach((item, index) => {
     if (index < questions.length - 2) {
-        // Previous logic for yes/no questions
+        
         let answer = prompt(item.question).toLowerCase();
         if (answer === 'yes') answer = 'y';
         if (answer === 'no') answer = 'n';
         if (answer === item.answer) {
             alert("Correct! Well done.");
+            correctAnswers++; // Increase the correct answers count
         } else {
             alert("Incorrect. The correct answer was " + (item.answer === 'y' ? 'yes' : 'no'));
         }
@@ -69,6 +72,10 @@ questions.forEach((item, index) => {
         if (!correct) {
             alert(`Sorry, you didn't guess the number. The correct answer was ${item.answer}.`);
         }
+        if (correct) {
+            correctAnswers++; // Increase the correct answers count
+        }
+
     } else {
         // Logic for multiple answers question
         let attempts = 6;
@@ -88,10 +95,23 @@ questions.forEach((item, index) => {
             attempts--;
         }
         if (!correct) {
-            alert(`Sorry, you didn't guess my favorite person. The correct answers were ${item.answers.join(", ")}.`);
+            // Constructing string of correct answers manually
+            let correctAnswersStr = "";
+            for(let i = 0; i < item.answers.length; i++) {
+                correctAnswersStr += item.answers[i];
+                if (i !== item.answers.length - 1) { // If it's not the last element
+                    correctAnswersStr += ", ";
+                }
+            }
+            alert(`Sorry, you didn't guess my favorite person. The correct answers were ${correctAnswersStr}.`);
+        }
+        if (correct) {
+            correctAnswers++; // Increase the correct answers count
         }
     }
+  
+    
 });
 
 // Display the user’s name back to them in the final message
-alert(`Congratulations, ${userName}! You have answered all the questions!`);
+alert(`Congratulations, ${userName}! You have answered correctly to ${correctAnswers} out of ${questions.length} questions!`);
